1d3f0d2dffcd2113bf22f8a684065a63b314053e
Ogonna Iwunze <ogonna.iwunze@gmail.com>
2016-03-10 23:19:45 +0000

    [OI] add integration test for s3 backend

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -7,3 +7,11 @@ end
 describe docker_container('registry') do
   it { should be_running }
 end
+
+describe docker_image('registry:2-s3') do
+    it { should exist }
+end
+
+describe docker_container('registry-s3') do
+  it { should be_running }
+end
